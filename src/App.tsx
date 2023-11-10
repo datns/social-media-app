3b62c545372692de3b5df891730bf51e2a616f91
dat.s.nguyen <dat.s.snguyen@taptap.com.vn>
2023-11-10 10:14:26 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './global.css';
+import {Route, Routes} from "react-router-dom";
+
+const App = () => {
+	return (
+		<main className="flex h-screen">
+			<Routes>
+				{/* public routes */}
+				<Route path="sign-in" element={<SigninForm />} />
+
+				{/* private routes */}
+				<Route index element={<Home />} />
+			</Routes>
+		</main>
+	)
+}
+
+export default App;
